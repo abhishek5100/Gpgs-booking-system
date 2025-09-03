4d99b65366205536527107bd47b7e9dca9a61638
AbhishekGautam <151998771+abhishek5100@users.noreply.github.com>
2025-09-03 15:49:32 +0530

    Update ConfirmationModel.jsx

--- src/components/ConfirmationModel.jsx
+++ src/components/ConfirmationModel.jsx
@@ -134,8 +134,10 @@ if (formPreviewData?.TempBedDOJ) {
     const balanceAmount =
       totalAmount - (AskForBAOrFA === "Booking_Amount " ? Number(PermBedMonthlyFixRent) : 0);
 
-    let msg = `Payment Details For ${ClientFullName}\n ( ${CallingNo} )`;
+    let msg = `Payment Details For ${ClientFullName} ( ${CallingNo} )/n    
     
+    `;
+
     if (TempPropCode) {
       msg += `
 Temporary PG Facility Code: ${TempPropCode}
